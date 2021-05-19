0b1124277f77cb33459efd72ea2c570219e6017c
Rabin Shrestha <shrestha.rabin1700@gmail.com>
2021-05-19 13:05:29 +0545

    2nd commit

---
+++ Area/AreaOfTriangle1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height,base;
+    float ans; //ans may come in fractions
+    cout<<"Enter height and base :";
+    cin>>height>>base;
+            
+            ans=(0.5)*height*base;
+    
+    cout<<"Area of triangle is:"<<ans;
+        return 0;
+}
